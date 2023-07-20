5ed5dbc29f5a78b7c528e823ec499697c468f936
Shahrukh khan <shahrukhkhan9912@gmail.com>
2023-07-20 19:44:08 +0530

    node first code push

---
+++ index.js
@@ -0,0 +1,66 @@
+const chalk = require("chalk");
+
+// Node JS first program Start
+
+// var a = 10;
+// var b = 30;
+// var c = 40;
+// console.log(a+b+c);
+
+// Node JS first program End
+
+// Node JS module import Export Start
+
+// const app = require("./app")
+
+// console.log(app);
+// console.log(app.x);
+// console.log(app.y);
+
+// Node JS module import Export End
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// Node JS Create Text File Start 
+
+// var first = require("fs");
+
+// first.writeFileSync("nodes.txt", "Learn By Code Step by Step")
+
+// Node JS Create Text File End 
+
+// Node JS Create Basic Server Start
+
+// var http = require("http");
+
+// http.createServer((req, res) => {
+//     // res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.write("<h1>Hello World</h1>");
+//     res.end();
+// }).listen(5000);
+
+// Node JS Create Basic Server End
+
+// Node JS Create Package.JSON File Start
+
+// const colors = require('colors');
+
+//     console.log("Use Color Property".black.bgWhite);
+
+// Node JS Create Package.JSON File End
+
+
+// Node JS Solve Problem Start
+
+// import chalk from "chalk";
+
+// const chalk = require("chalk");
+
+console.log(chalk.blue("Testing Chalk Package"));
+// Node JS Solve Problem End
+
+
+
+
